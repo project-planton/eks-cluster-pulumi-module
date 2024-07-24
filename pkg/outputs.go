abf998ada8eb59f2d6fb362b9308f1c66902ed8d
Swarup Donepudi <swarup.donepudi@gmail.com>
2024-07-24 20:26:12 +0530

    initialized

---
+++ pkg/outputs.go
@@ -0,0 +1,11 @@
+package pkg
+
+import (
+	"github.com/plantoncloud/planton-cloud-apis/zzgo/cloud/planton/apis/code2cloud/v1/aws/ekscluster/model"
+	"github.com/pulumi/pulumi/sdk/v3/go/auto"
+)
+
+func PulumiOutputsToStackOutputsConverter(pulumiOutputs auto.OutputMap,
+	input *model.EksClusterStackInput) *model.EksClusterStackOutputs {
+	return &model.EksClusterStackOutputs{}
+}
