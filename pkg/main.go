e8bee3a9092c449418873524dc282c09e70c1c72
Swarup Donepudi <swarup.donepudi@gmail.com>
2024-08-13 14:58:27 +0530

    refactored for protos in one package per api resource

--- pkg/main.go
+++ pkg/main.go
@@ -2,13 +2,13 @@ package pkg
 
 import (
 	"github.com/pkg/errors"
-	"github.com/plantoncloud/planton-cloud-apis/zzgo/cloud/planton/apis/code2cloud/v1/aws/ekscluster/model"
+	"github.com/plantoncloud/planton-cloud-apis/zzgo/cloud/planton/apis/code2cloud/v1/aws/ekscluster"
 	"github.com/plantoncloud/pulumi-module-golang-commons/pkg/provider/aws/pulumiawsprovider"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
 type ResourceStack struct {
-	Input  *model.EksClusterStackInput
+	Input  *ekscluster.EksClusterStackInput
 	Labels map[string]string
 }
 
--- pkg/outputs/outputs.go
+++ pkg/outputs/outputs.go
@@ -1,11 +1,11 @@
 package outputs
 
 import (
-	"github.com/plantoncloud/planton-cloud-apis/zzgo/cloud/planton/apis/code2cloud/v1/aws/ekscluster/model"
+	"github.com/plantoncloud/planton-cloud-apis/zzgo/cloud/planton/apis/code2cloud/v1/aws/ekscluster"
 	"github.com/pulumi/pulumi/sdk/v3/go/auto"
 )
 
 func PulumiOutputsToStackOutputsConverter(pulumiOutputs auto.OutputMap,
-	input *model.EksClusterStackInput) *model.EksClusterStackOutputs {
-	return &model.EksClusterStackOutputs{}
+	input *ekscluster.EksClusterStackInput) *ekscluster.EksClusterStackOutputs {
+	return &ekscluster.EksClusterStackOutputs{}
 }
