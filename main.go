64f0ee0f006efa3303e1c4843201168dd5e2e040
Swarup Donepudi <swarup.donepudi@gmail.com>
2024-10-12 16:52:10 +0530

    moved to project-planton

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	eksclusterv1 "buf.build/gen/go/plantoncloud/project-planton/protocolbuffers/go/project/planton/provider/aws/ekscluster/v1"
+	eksclusterv1 "buf.build/gen/go/project-planton/apis/protocolbuffers/go/project/planton/provider/aws/ekscluster/v1"
 	"github.com/pkg/errors"
-	"github.com/plantoncloud/eks-cluster-pulumi-module/pkg"
-	"github.com/plantoncloud/pulumi-module-golang-commons/pkg/stackinput"
+	"github.com/project-planton/eks-cluster-pulumi-module/pkg"
+	"github.com/project-planton/pulumi-module-golang-commons/pkg/stackinput"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
