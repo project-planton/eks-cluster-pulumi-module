87ed1260892bdaa163d42e1940bbc564b732d421
Swarup Donepudi <swarup.donepudi@gmail.com>
2024-11-09 17:57:08 +0530

    switched over to using local generated stubs for project-planton apis

--- pkg/main.go
+++ pkg/main.go
@@ -1,8 +1,8 @@
 package pkg
 
 import (
-	eksclusterv1 "buf.build/gen/go/project-planton/apis/protocolbuffers/go/project/planton/provider/aws/ekscluster/v1"
 	"github.com/pkg/errors"
+	eksclusterv1 "github.com/project-planton/project-planton/apis/go/project/planton/provider/aws/ekscluster/v1"
 	"github.com/pulumi/pulumi-aws/sdk/v6/go/aws"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
